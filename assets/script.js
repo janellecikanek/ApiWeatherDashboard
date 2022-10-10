61724b225002a8b79b0c1076188dd12f5dee8583
Janelle Cikanek <janellecikanek@gmail.com>
2022-10-09 20:12:06 -0500

    updated 5 day java script to go from 3 hours to 24

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,90 @@
 var ApiKey = "c7eeeb41361234b4d229a78f7b609b2d";
-var userForm = document.querySelector('#user-form');
+var userForm = document.querySelector("#user-form");
 var btnPrimary = document.querySelector(".btn-primary");
-var cityInput = document.querySelector('#city');
-var currentTemp = document.querySelector('#currentTemp');
-var searchCity = document.querySelector('#searchCity');
+var cityInput = document.querySelector("#city");
+var currentTemp = document.querySelector("#currentTemp");
+var searchCity = document.querySelector("#searchCity");
 
+var formSubmitHandler = function (event) {
+  event.preventDefault();
 
+  var city = cityInput.value.trim();
 
-    var formSubmitHandler = function (event) {
-        event.preventDefault();
-      
-    var city = cityInput.value.trim();
+  if (city) {
+    getCityForecast(city);
 
-    if (city) {
-        getcityForecast(city);
-    
-       currentTemp.textContent = '';
-        cityInput.value = '';
+    currentTemp.textContent = "";
+    cityInput.value = "";
+  } else {
+    alert("Please enter a city name");
+  }
+};
 
-      } else {
-        alert('Please enter a city name');
-      };
-    }
+var getCityForecast = function searchApi() {
+  var locQueryUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=minneapolis&appid=" +
+    ApiKey +
+    "&units=imperial";
 
-function searchApi() {
-    var locQueryUrl =
-        "https://api.openweathermap.org/data/2.5/forecast?q=minneapolis&appid=" +
-        ApiKey + "&units=imperial";
-    fetch(locQueryUrl)
-        .then((res) => res.json())
-        .then((res) => {
-            console.log(res);
-            let lon = res.city.coord.lon;
-            let lat = res.city.coord.lat;
-            let cityID = res.city.id;
-            console.log("lat: " + lat, "Lon: " + lon);
+  // "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + ApiKey
 
-            function forecast() {
-                var forecastUrl =
-                    "https://api.openweathermap.org/data/2.5/forecast?id=" +
-                    cityID +
-                    "&appid=" +
-                    ApiKey;
-                fetch(forecastUrl)
-                    .then((res) => res.json())
-                    .then((res) => {
-                        console.log(res.list.slice(0, 5));
-                    });
-            }
-            forecast();
-        });
-}
+  fetch(locQueryUrl)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      let lon = res.city.coord.lon;
+      let lat = res.city.coord.lat;
+      let cityID = res.city.id;
+      console.log("lat: " + lat, "Lon: " + lon);
+      // displayWeather(res, city);
 
-    
-searchApi();
+      // function forecast() {
+      //     var forecastUrl =
+      //         "https://api.openweathermap.org/data/2.5/forecast?id=" +
+      //         cityID +
+      //         "&appid=" +
+      //         ApiKey;
+      //     fetch(forecastUrl)
+      //         .then((res) => res.json())
+      //         .then((res) => {
+      //             console.log(res.list.slice(0, 5));
+      //         });
+      // }
 
-userForm.addEventListener('submit', formSubmitHandler);
+      function forecast(lat, lon) {
+        var forecastUrl =
+          "https://api.openweathermap.org/data/2.5/forecast?id=" +
+          cityID +
+          "&appid=" +
+          ApiKey +
+          "&units=imperial";
+        fetch(forecastUrl)
+          .then((res) => res.json())
+          .then((res) => {
+            var fiveDay = [];
+            fiveDay.push(
+              data.list[6],
+              data.list[14],
+              data.list[22],
+              data.list[30],
+              data.list[38]
+            );
+          });
+      }
+
+      forecast();
+    });
+};
+
+// searchApi();
+
+userForm.addEventListener("submit", formSubmitHandler);
 
 // https://github.com/jnordan132/City-Weather-Dashboard/blob/main/script.js
 
-
-
 // let storrageArray []
 
-
-// function getApi happens each time. local storage happens once 
+// function getApi happens each time. local storage happens once
 // add local storage to ApiKey
 
 // letlocationarray=json.parse(localstorage.getitem("search"))
@@ -74,7 +94,7 @@ userForm.addEventListener('submit', formSubmitHandler);
 
 // for loop inside it use java or jquesry
 // create elements and apend to URL
-// for loop itterate through 
+// for loop itterate through
 // var i = 0, i<locationarray.lenght; i++0{
 
 // }
@@ -82,42 +102,36 @@ userForm.addEventListener('submit', formSubmitHandler);
 // new search button =document.createElement.(button)
 // searchbutton.textcontent=locationArray[i];
 // append to parent element
-// SpeechRecognitionResultList.appendsearch button  = empty on load search history appear on search event. 
+// SpeechRecognitionResultList.appendsearch button  = empty on load search history appear on search event.
 // set class attribute
 // searchbtn.setattribute(class, btn, btn-lg)
 // recentllist.append(searchbtn);
 
-// to appear on load: storage array duplicate logic to run on load or pull logic into a function and use it 
-// copy the loop and drop it right after the local storage call  
+// to appear on load: storage array duplicate logic to run on load or pull logic into a function and use it
+// copy the loop and drop it right after the local storage call
 
-
-// on submit event need to clear the element. before the for loop 
+// on submit event need to clear the element. before the for loop
 
 // recentlist.empty(): in front of for loop
-
 
 // this will need to be a function. pulling out fetch logic (fetch city)then do the fetch logic
 
 // event delegation for dynamically created Event
 // write logic for that event.target get its text value plug into created function
 
+// function uvApi() {
+//     var uvUrl =
+//         "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" +
+//         lat +
+//         "&lon=" +
+//         lon +
+//         "&appid=" +
+//         ApiKey +
+//         "&cnt=1";
 
-            // function uvApi() {
-            //     var uvUrl =
-            //         "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" +
-            //         lat +
-            //         "&lon=" +
-            //         lon +
-            //         "&appid=" +
-            //         ApiKey +
-            //         "&cnt=1";
-
-                // fetch(uvUrl)
-                //     .then((res) => res.json())
-                //     .then((res) => {
-                //         console.log(res);
-                //         var uvIndex = res[0].value;
-                //         console.log(uvIndex);
-
-
-        
+// fetch(uvUrl)
+//     .then((res) => res.json())
+//     .then((res) => {
+//         console.log(res);
+//         var uvIndex = res[0].value;
+//         console.log(uvIndex);
